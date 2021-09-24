4529d1da881b52a4a70a4ed5b5a66f91e7ddc244
nbanker <originalbanker@gmail.com>
2021-09-24 11:52:46 +0700

    global prettier format

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
-require('dotenv').config();
+require('dotenv').config()
 
 const cmt = process.env.TEST_COMMENT
 
-
-app.get('/', (req, res)=>{
-	res.send(`${cmt}`)
+app.get('/', (req, res) => {
+    res.send(`${cmt}`)
 })
 
 app.listen(3000)
